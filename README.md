500c3546a809f38d068162657a301777f79d8990
Keith Estanislao <kestanislao09@outlook.com>
2022-02-04 03:49:18 -0800

    Calculator works!

--- README.md
+++ README.md
@@ -1,17 +1,37 @@
 # calculatron-3000
 Simple calculator  for The Odin Project
 
-Final Project for The Odin Project: Foundations Curriculum!
+Final Project for **The Odin Project: Foundations Curriculum!**
 
-Reinforcing all of the concepts I've mastered so far..
+## Reinforcing all of the concepts I've mastered so far in..
 
-HTML
-CSS
-JS
+ - HTML
+ - CSS
+ - JS
 
-Pulling out all(not really) the stops to make an awesome calculator!
+Pulling out ~~all~~ *(not really)* the stops to make an awesome calculator!
 
-I'm assuming that ill be using:
--Functions
--DOM Manipulation
--HTML and CSS basics
+I'm assuming that I'll be using:
+ - Functions
+ - DOM Manipulation
+ - HTML and CSS basics
+ - Markdown syntax for this README
+
+## **Things to do**
+1. Fix some minor details with the current CSS
+     - Viewport changes size if there is nothing displayed.
+     - Need to re-do the CSS grid to acommodate the extra credit items below.
+
+2. Extra credit
+     - Implement a decimal button.
+     - Implement a backspace button
+     - Keyboard support
+
+3. Clean up the code
+
+4. Pretti-fy the CSS
+
+## **UPDATES**
+
+## 2/4/22 0335
+ - Finally got the calculator into working order. Logic was tough to figure out, but with (alot) of tinkering I was able to get the numbers to store properly in the variables, and operate on them in a way that makes sense. Code needs **a-f---in-Lot** of optimization and cleaning. But the main thing is that it works. 
--- script.js
+++ script.js
@@ -14,23 +14,114 @@ function div(a, b) {
     return a / b;
 };
 
-
 function operate(a, opr, b) {
     switch(opr){
         default:
             console.log('something went wrong..');
             break;
-        case(add):
-            add(a, b);
+        case('+'):
+            return add(a, b);
             break;
-        case(sub):
-            sub(a, b);
+        case('-'):
+            return sub(a, b);
             break;
-        case(mul):
-            mul(a, b)
+        case('x'):
+            return mul(a, b)
             break;
-        case(div);
-            div(a, b);
+        case('/'):
+            return div(a, b);
     };
 
 }
+
+//querySelectors 
+const numButtons = document.querySelectorAll('.num');
+const oprButtons = document.querySelectorAll('.opr');
+const viewPort = document.querySelector('#viewport');
+const clButton = document.querySelector('.clear');
+
+//Array to store current display number on the screen
+let dispNum = [];
+let prevNumber = [0];
+let currentNum
+let operator = '';
+let reUse = 0;
+
+function convertFromArray(arr) {
+    return parseInt(arr.join(''))
+}
+let dispConverted = convertFromArray(dispNum);
+
+function clearDisplay() {
+    dispNum = [];
+    viewPort.textContent = '';
+    reUse = 0;
+}
+
+oprButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let buttonContent = e.target.textContent;
+        if (buttonContent === '=' && operator === '') {
+            viewPort.textContent = convertFromArray(dispNum);
+        } else if (operator === '') {
+            operator = buttonContent;
+            previousNum = dispNum;
+            clearDisplay();
+        } else if (operator !== '') {
+            let current
+            let previous
+            
+            Array.isArray(previousNum) ? previous = parseInt(previousNum.join('')) : previous = previousNum;
+            
+            if (isNaN(convertFromArray(dispNum))) {
+                viewPort.textContent = 'error'
+                reUse = 1
+                return;
+            } else {
+                current = convertFromArray(dispNum);
+            };
+            
+            let operatedNum = operate(previous, operator, current);
+            let operatedArr = operatedNum.toString().split('');
+            if (operatedNum > 999999) {
+                operatedNum = operatedNum.toExponential(4)
+            } else if (operatedNum <= 999999 && operatedArr.length > 8) {
+                console.log(operatedArr)
+                operatedNum = operatedArr.splice(8, operatedArr.length-8).join('')
+            }
+            
+            viewPort.textContent = operatedNum;
+            
+            if (buttonContent === '=') {
+                previousNum = operatedNum;
+                reUse = 1;
+            }  else if (reUse === 1) {
+                operator = buttonContent;
+                clearDisplay();
+                reUse = 0;
+            } else {
+                operator = buttonContent;
+                dispNum = [];
+                previousNum = operatedNum;
+            };
+        };
+    });
+});
+
+numButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let buttonContent = e.target.textContent;
+        if (reUse === 1) {
+            clearDisplay();
+            operator = '';
+        }
+        dispNum.push(buttonContent);
+        viewPort.textContent = dispNum.join('');
+    });
+});
+
+clButton.addEventListener('click', (e) => {
+    clearDisplay();
+    operator = '';
+    prevNumber = [];
+});
